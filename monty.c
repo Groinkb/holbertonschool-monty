eee74cfc2be1a3e4387d14c83c3d0ea5abf03529
Groinkb <benjamin@mossu.ch>
2023-09-08 18:54:50 +0200

    2 new functions to work main code on branch main

--- monty.c
+++ monty.c
@@ -1,88 +1,30 @@
 #include "monty.h"
+global_var var_global; /* Declare a global variable of type global_var */
 
-#define STACK_SIZE 100
+/**
+ * main - The main driver function for the monty program.
+ * @ac: The number of command-line arguments.
+ * @av: An array of strings containing the command-line arguments.
+ * Return: 0 on successful execution, or an error code on failure.
+ */
+int main(int ac, char **av)
+{
+	stack_t *stack; /* Declare a pointer to a stack_t structure */
 
-typedef struct Stack {
-    int items[STACK_SIZE];
-    int top;
-} Stack;
+	stack = NULL; /* Initialize the stack pointer to NULL */
 
-Stack stack;
+	/* Check if the number of command-line arguments is not 2 */
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n"); /* Print an error message to stderr */
+		exit(EXIT_FAILURE); /* Exit the program with a failure status code */
+	}
 
-void push(int value, int line_number) {
-    if (stack.top >= STACK_SIZE - 1) {
-        printf("L%d: can't push, stack overflow\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    stack.items[++stack.top] = value;
+	read_file(av[1], &stack); /* Call the read_file function to process the specified file */
+
+	/* Remember to free memory */
+	free_dlistint(stack); /* Call the free_dlistint function to free memory associated with the stack */
+
+	return (0); /* Return 0 to indicate successful execution */
 }
 
-void pall() {
-    for (int i = stack.top; i >= 0; i--) {
-        printf("%d\n", stack.items[i]);
-    }
-}
-
-int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        printf("USAGE: monty file\n");
-        return EXIT_FAILURE;
-    }
-
-    FILE *file = fopen(argv[1], "r");
-    if (!file) {
-        printf("Error: Can't open file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
-
-    char line[256];
-    int line_number = 0;
-    int pall_triggered = 0;
-
-    while (fgets(line, sizeof(line), file)) {
-        line_number++;
-
-        char *trimmed_line = strtok(line, "\t\n");
-        if (!trimmed_line) {
-            continue;
-        }
-
-        if (trimmed_line[0] == '#') {
-            continue;
-        }
-
-        char *opcode = strtok(trimmed_line, " ");
-
-        if (!opcode)
-            continue;
-
-        if (strcmp(opcode, "push") == 0) {
-            char *arg = strtok(NULL, " \t\n");
-            if (arg) {
-                int arg_length = strlen(arg);
-                if (arg_length > 0 && arg[arg_length - 1] == '$') {
-                    arg[arg_length - 1] = '\0';
-                }
-                push(atoi(arg), line_number);
-            } else {
-                printf("L%d: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-            }
-        } else if (strcmp(opcode, "pall$") == 0) {
-            if (!pall_triggered) {
-                pall();
-                pall_triggered = 1;
-            }
-        } else {
-            printf("L%d: unknown instruction %s\n", line_number, opcode);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    if (!pall_triggered) {
-        pall();
-    }
-
-    fclose(file);
-    return EXIT_SUCCESS;
-}
